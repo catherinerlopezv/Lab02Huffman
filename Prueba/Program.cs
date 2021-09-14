dc753c3bb17c7146284be9d767590c878ca68338
Catherine <catherinerlopezv@gmail.com>
2021-09-14 10:05:09 -0600

    Add project files.

---
+++ Prueba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribir texto: ");
+            string texto = Console.ReadLine();
+            using (StreamWriter outputFile = new StreamWriter("Prueba.txt"))
+            {
+                outputFile.WriteLine(texto);
+            }
+            Operaciones imp = new Operaciones("Prueba.txt", "Arbol");
+            imp.Comprimir();
+            imp.Descomporimir();
+        }
+    }
+}
