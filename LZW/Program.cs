0a39a96ae680697009742d8642f0848148b0353a
Zroeth <31348558+Zroeth@users.noreply.github.com>
2021-09-27 15:53:51 -0600

    Add files via upload

---
+++ LZW/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace LZW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribir texto: ");
+            string texto = Console.ReadLine();
+            using (StreamWriter outputFile = new StreamWriter("Prueba.txt"))
+            {
+                outputFile.WriteLine(texto);
+            }
+            OperacionesH imp = new OperacionesH("Prueba.txt", "Arbol");
+            imp.Comprimir();
+            imp.Descomprimir();
+
+
+
+            Console.Write("LZW");
+            OperacionesL impLZW = new OperacionesL("Prueba.txt", "Arbol");
+
+            impLZW.Comprimir();
+            impLZW.Descomprimir();
+
+        }
+    }
+}
---
+++ LZW/Serializar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace LZW
+{
+    internal class Serializar
+    {
+        internal static void GuardarArchivoBinario<T>(string ubicacionArchivo, T Contenido)
+        {
+            try
+            {
+                using (var cadenaM = new MemoryStream())
+                {
+                    var formatBin = new BinaryFormatter();
+
+                    formatBin.Serialize(cadenaM, Contenido);
+
+                    string ubicacion = Path.GetDirectoryName(ubicacionArchivo);
+
+                    if (!Directory.Exists(ubicacion))
+                    {
+                        Directory.CreateDirectory(ubicacion);
+                    }
+                    File.WriteAllBytes(ubicacionArchivo, cadenaM.ToArray());
+
+                    cadenaM.Close();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        internal static T DeserializarArchivoBinario<T>(string ubicacionArchivo)
+        {
+            BinaryFormatter formato = new BinaryFormatter();
+            using (var cadenaM = new MemoryStream(File.ReadAllBytes(ubicacionArchivo)))
+            {
+                cadenaM.Position = 0;
+                return (T)formato.Deserialize(cadenaM);
+            }
+        }
+    }
+}
+
