7934f16b75f763d5c4da95ef10f505de1c662eda
Zroeth <31348558+Zroeth@users.noreply.github.com>
2021-09-28 16:41:00 -0600

    Add files via upload

--- Lab3/Data/Informacion.cs
+++ Lab3/Data/Informacion.cs
@@ -15,33 +15,40 @@ namespace Lab3
 
         private Informacion()
         {
-           
-            double factor=0,razon=0,porcentaje=0;         
-            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\wwwroot\Upload\FactorRazon.txt");
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\wwwroot\Upload\.txt");
             var lines = File.ReadAllLines(path);
-   
-            for (var i = 0; i < lines.Length; i += 1)
-            {
-                razon = Convert.ToDouble(lines[0]);
-                factor = Convert.ToDouble(lines[1]);
-                porcentaje = Convert.ToDouble(lines[2]);
-                break;
-            }
+            long ArchO = lines.Length;
+            string path2 = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\wwwroot\Upload\.huff");
+            var lines2 = File.ReadAllLines(path);
+            long ArchC = lines.Length;
+
+            long razon = ArchC / ArchO;
+            long factor = ArchO / ArchC;
+            long porcentaje = factor * 100;
+
+            /* for (var i = 0; i < lines.Length; i += 1)
+             {
+                 razon = Convert.ToDouble(lines[0]);
+                 factor = Convert.ToDouble(lines[1]);
+                 porcentaje = Convert.ToDouble(lines[2]);
+                 break;
+             }*/
 
 
             informacion = new List<Json>();
-                Json newExchangeRate = new Json
-                {
-                    nombreArchivo = "",
-                    ubicacionComprimido= "",
-                    factorCompresion = factor,
-                    razonCompresion = razon,
-                    porcentajeCompresion=porcentaje
+            Json newExchangeRate = new Json
+            {
+                nombreArchivo = "",
+                ubicacionComprimido = path2,
+                factorCompresion = factor,
+                razonCompresion = razon,
+                porcentajeCompresion = porcentaje
 
-                };
-                informacion.Add(newExchangeRate);
+            };
+            informacion.Add(newExchangeRate);
 
-               
+
+
         }
 
         public static Informacion Instance
---
+++ Lab3/Data/Jsonlzw.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Jsonlzw
+    {
+        public string nombreArchivolzw { get; set; }
+        public string ubicacionComprimidolzw { get; set; }
+        public double razonCompresionlzw { get; set; }
+        public double factorCompresionlzw  { get; set; }
+        public double porcentajeCompresionlzw { get; set; }
+
+    }
+}
